8885690c7ec053ae9e302be35e2da4d0fb64f383
Dominika <dominika.gredka@edu.uekat.pl>
2024-02-27 23:20:34 +0100

    Zad 4

---
+++ Zad4.py
@@ -0,0 +1,8 @@
+# Stworzyć funkcję, która przyjmie 3 argumenty typu int i sprawdzi czy suma
+# dwóch pierwszych liczb jest większa lub równa trzeciej, a następnie zwróci tę
+# informację jako typ logiczny bool
+
+def funkcja(a, b, c):
+    return  a + b >= c
+
+print(funkcja(1,3,2))
