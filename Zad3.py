2e7eb7498d2c8b1067d56d272ba3b8279d050d43
Dominika <dominika.gredka@edu.uekat.pl>
2024-02-27 23:16:11 +0100

    Zad 3

---
+++ Zad3.py
@@ -0,0 +1,15 @@
+# Stworzyć funkcję, która będzie sprawdzała czy przekazana liczba w
+# parametrze jest parzysta i zwróci tą informację jako typ logiczny bool
+# ( True / False ). Należy uruchomić funkcję, wynik wykonania zapisać do
+# zmiennej, a następnie wykorzystując warunek logiczny wyświetlić prawidłowy
+# tekst "Liczba parzysta" / "Liczba nieparzysta"
+
+def funkcja(liczba):
+    return liczba % 2 == 0
+
+czyParzysta = funkcja(4)
+
+if czyParzysta:
+    print("Liczba parzysta")
+else:
+    print("Liczba nieparzysta")
